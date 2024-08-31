2dcf929134bc498c00cefd6cc03575d47b5f2c12
Karm Patel <contact.karmpatel@gmail.com>
2024-08-31 13:21:52 +0530

    Remove All

--- frontend/src/Components/Minibar.jsx
+++
@@ -1,9 +0,0 @@
-import { NavLink } from "react-router-dom"
-import { Box } from "@chakra-ui/react"
-export const Minibar=()=>{
-    return(
-        <Box display={"flex"} gap="10px">
-            <NavLink style={({ isActive }) =>isActive? { color: 'green',fontSize:"12px" }: { color:"black",fontSize:"11px" }} to="/furniture">Furnitures</NavLink>
-        </Box>
-    )
-}
